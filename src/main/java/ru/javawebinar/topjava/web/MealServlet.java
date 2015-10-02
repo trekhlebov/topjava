81754c417f1a2dbebd8b8e4da31f1de37303af10
trekhlebov <trekhlebov@gmail.com>
2015-10-02 20:18:02 +0300

    Display ExceedMeals in colors

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,10 +1,19 @@
 package ru.javawebinar.topjava.web;
 
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.model.UserMealWithExceed;
+import ru.javawebinar.topjava.util.UserMealsUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by holonavt on 23.09.15.
@@ -12,6 +21,23 @@ import java.io.IOException;
 public class MealServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<UserMeal> mealList = Arrays.asList(
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510),
+                new UserMeal(LocalDateTime.of(2015, Month.JUNE, 01, 10, 15), "Завтрак", 700),
+                new UserMeal(LocalDateTime.of(2015, Month.JUNE, 01, 13, 20), "Обед", 1100),
+                new UserMeal(LocalDateTime.of(2015, Month.JUNE, 01, 19, 10), "Ужин", 800)
+        );
+
+        List<UserMealWithExceed> filteredWithExceed =
+                UserMealsUtil.getFilteredMealsWithExceededByCycle(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+
+        request.setAttribute("mealList", filteredWithExceed);
 
         request.getRequestDispatcher("/mealList.jsp").forward(request, response);
     }
--- src/main/webapp/mealList.jsp
+++ src/main/webapp/mealList.jsp
@@ -6,11 +6,22 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
     <title>Meal list</title>
 </head>
 <body>
     <h2>Meal list</h2>
+
+
+    <c:forEach var="meal" items="${mealList}">
+        <c:if test="${meal.exceed}">
+            <p style="color:red">${meal}</p>
+        </c:if>
+        <c:if test="${!meal.exceed}">
+            <p style="color:#2aff4d">${meal}</p>
+        </c:if>
+    </c:forEach>
 </body>
 </html>
