67fff846c4b665cb9140bf4cdf322c9bcfb6c233
trekhlebov <trekhlebov@gmail.com>
2015-10-24 15:17:01 +0300

    Добавил поле id и мете.

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -11,6 +11,9 @@ import ru.javawebinar.topjava.util.TimeUtil;
  * 11.01.2015.
  */
 public class UserMeal {
+
+    protected Integer id;
+
     protected final LocalDateTime dateTime;
 
     protected final String description;
@@ -18,9 +21,26 @@ public class UserMeal {
     protected final int calories;
 
     public UserMeal(LocalDateTime dateTime, String description, int calories) {
+        this(null, dateTime, description, calories);
+    }
+
+    public UserMeal(Integer id, LocalDateTime dateTime, String description, int calories) {
+        this.id = id;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public boolean isNew(){
+        return id ==null;
     }
 
     public LocalDateTime getDateTime() {
@@ -37,5 +57,15 @@ public class UserMeal {
 
     public boolean isBetween(LocalTime startTime, LocalTime endTime){
         return TimeUtil.isBetween(LocalTime.from(this.dateTime), startTime, endTime);
+    }
+
+    @Override
+    public String toString(){
+        return "UserMeal{" +
+                "id=" + id +
+                ", dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                '}';
     }
 }
