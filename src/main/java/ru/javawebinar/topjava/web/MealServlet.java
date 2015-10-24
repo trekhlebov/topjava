9637847bd5fcc3f511b03bad6bf76749ca92d0f3
trekhlebov <trekhlebov@gmail.com>
2015-10-24 18:10:36 +0300

    Сделал в сервлете реализацию методов Get и Post.

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,6 +1,9 @@
 package ru.javawebinar.topjava.web;
 
 import ru.javawebinar.topjava.LoggerWrapper;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.repository.InMemoryUserMealRepository;
+import ru.javawebinar.topjava.repository.UserMealRepository;
 import ru.javawebinar.topjava.util.UserMealsUtil;
 
 import javax.servlet.ServletException;
@@ -8,18 +11,67 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * Created by holonavt on 23.09.15.
  */
+
+// TODO Добавить mealEdit.jsp из ветки master
+
 public class MealServlet extends HttpServlet {
+
+    private UserMealRepository repository;
 
     private static final LoggerWrapper LOG = LoggerWrapper.get(MealServlet.class);
 
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        repository = new InMemoryUserMealRepository();
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
 
-        request.setAttribute("mealList", UserMealsUtil.getWithExceeded(UserMealsUtil.MEAL_LIST, 2000));
+        if (action == null) {
+            LOG.info("getAll");
+            request.setAttribute("mealList", UserMealsUtil.getWithExceeded(repository.getAll(), 2000));
+            request.getRequestDispatcher("/mealList.jsp").forward(request, response);
+        } else if (action.equals("delete")) {
+            int id = getId(request);
+            LOG.info("delete");
+            repository.delete(id);
+            response.sendRedirect("meals");
+        }
+        else {
+            final UserMeal meal = action.equals("create") ?
+                    new UserMeal(LocalDateTime.now(), "", 1000) :
+                    repository.get(getId(request));
 
-        request.getRequestDispatcher("/mealList.jsp").forward(request, response);
+            request.setAttribute("meal", meal);
+            request.getRequestDispatcher("mealEdit.jsp").forward(request, response);
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String id = request.getParameter("id");
+
+        UserMeal userMeal = new UserMeal(id.isEmpty() ? null : Integer.valueOf(id),
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.valueOf(request.getParameter("calories")));
+        LOG.info(userMeal.isNew() ? "Create{}" : "Update{}", userMeal);
+
+        repository.save(userMeal);
+
+        response.sendRedirect("meals");
+    }
+
+    private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.valueOf(paramId);
     }
 }
