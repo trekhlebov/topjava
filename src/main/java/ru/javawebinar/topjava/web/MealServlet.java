029fd045c000b77bf0c460b10ed19e858f3d5df5
trekhlebov <trekhlebov@gmail.com>
2015-10-24 13:59:00 +0300

    Доработал серврлет и JSP.

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.topjava.web;
 
-import ru.javawebinar.topjava.model.UserMeal;
-import ru.javawebinar.topjava.model.UserMealWithExceed;
+import ru.javawebinar.topjava.LoggerWrapper;
 import ru.javawebinar.topjava.util.UserMealsUtil;
 
 import javax.servlet.ServletException;
@@ -9,35 +8,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by holonavt on 23.09.15.
  */
 public class MealServlet extends HttpServlet {
 
+    private static final LoggerWrapper LOG = LoggerWrapper.get(MealServlet.class);
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        List<UserMeal> mealList = Arrays.asList(
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510),
-                new UserMeal(LocalDateTime.of(2015, Month.JUNE, 01, 10, 15), "Завтрак", 700),
-                new UserMeal(LocalDateTime.of(2015, Month.JUNE, 01, 13, 20), "Обед", 1100),
-                new UserMeal(LocalDateTime.of(2015, Month.JUNE, 01, 19, 10), "Ужин", 800)
-        );
-
-        List<UserMealWithExceed> filteredWithExceed =
-                UserMealsUtil.getFilteredMealsWithExceededByCycle(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
-
-        request.setAttribute("mealList", filteredWithExceed);
+        request.setAttribute("mealList", UserMealsUtil.getWithExceeded(UserMealsUtil.MEAL_LIST, 2000));
 
         request.getRequestDispatcher("/mealList.jsp").forward(request, response);
     }
--- src/main/webapp/mealList.jsp
+++ src/main/webapp/mealList.jsp
@@ -5,23 +5,46 @@
   Time: 21:24
   To change this template use File | Settings | File Templates.
 --%>
+<%@ page import="ru.javawebinar.topjava.util.TimeUtil" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <html>
 <head>
     <title>Meal list</title>
+    <style>
+        .normal {color: green;}
+        .exceeded {color: red;}
+    </style>
+
 </head>
 <body>
-    <h2>Meal list</h2>
+<section>
+    <h2><a href="index.html">Home</a></h2>    <h2>Meal list</h2>
+    <h3>Meal list</h3>
 
+    <table border="1" cellpadding="8" cellspacing="0">
+        <thead>
+        <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Calories</th>
+        </tr>
+        </thead>
 
     <c:forEach var="meal" items="${mealList}">
-        <c:if test="${meal.exceed}">
-            <p style="color:red">${meal}</p>
-        </c:if>
-        <c:if test="${!meal.exceed}">
-            <p>${meal}</p>
-        </c:if>
+        <jsp:useBean id="meal" scope="page" type="ru.javawebinar.topjava.model.UserMealWithExceed"/>
+        <tr class="${meal.exceed ? 'exceeded' : 'normal'}">
+            <td>
+                    <%--<fmt:parseDate value="${meal.dateTime}" pattern="y-M-dd'T'H:m" var="parsedDate"/>--%>
+                    <%--<fmt:formatDate value="${parsedDate}" pattern="yyyy.MM.dd HH:mm" />--%>
+                <%=TimeUtil.toString(meal.getDateTime())%>
+            </td>
+            <td>${meal.description}</td>
+            <td>${meal.calories}</td>
+        </tr>
     </c:forEach>
+    </table>
+</section>
 </body>
 </html>
